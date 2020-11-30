723d9f31c4c3133931436c22d5f9f5b796ac3b29
coderoo7 <shubhamyadav.70007@gmail.com>
2020-11-30 15:55:26 +0530

    render view test

--- photo_gallery/photo_gallery/settings.py
+++ photo_gallery/photo_gallery/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = BASE_DIR.joinpath('templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'photo_gallery.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

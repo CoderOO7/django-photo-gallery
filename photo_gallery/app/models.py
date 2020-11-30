99a1c363771ca9afcb0dcf006100369bae9c4101
coderoo7 <shubhamyadav.70007@gmail.com>
2020-11-30 21:11:09 +0530

    register image model

--- photo_gallery/app/models.py
+++ photo_gallery/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from taggit.managers import TaggableManager
 # Create your models here.
+
+class GalleryImage(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=80)
+    summary = models.CharField(max_length=255)
+    tags = TaggableManager()
